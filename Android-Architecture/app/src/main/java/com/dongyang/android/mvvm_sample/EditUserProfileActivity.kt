81eb1b3d88c243903bcdbbf00c7db9921031351a
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2021-12-13 20:38:41 +0900

    clone : LiveData

---
+++ Android-Architecture/app/src/main/java/com/dongyang/android/mvvm_sample/EditUserProfileActivity.kt
@@ -0,0 +1,38 @@
+package com.dongyang.android.mvvm_sample
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.dongyang.android.mvvm_sample.databinding.ActivityEditUserProfileBinding
+import com.dongyang.android.mvvm_sample.databinding.ActivityMainBinding
+
+class EditUserProfileActivity : AppCompatActivity() {
+    private val binding : ActivityEditUserProfileBinding by lazy {
+        ActivityEditUserProfileBinding.inflate(layoutInflater)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val phone = intent.getStringExtra("phone")
+        val name = intent.getStringExtra("name")
+
+        binding.edtPhone.setText(phone)
+        binding.edtName.setText(name)
+
+        binding.edtButton.setOnClickListener {
+            Log.d("EditUser", "Callback")
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("phone", binding.edtPhone.text.toString())
+            intent.putExtra("name", binding.edtName.text.toString())
+            setResult(Activity.RESULT_OK, intent) // 값을 보냄
+            finish()
+        }
+
+    }
+}
--- Android-Architecture/app/src/main/java/com/dongyang/android/mvvm_sample/MainActivity.kt
+++ Android-Architecture/app/src/main/java/com/dongyang/android/mvvm_sample/MainActivity.kt
@@ -1,27 +1,55 @@
 package com.dongyang.android.mvvm_sample
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.MutableLiveData
 import com.dongyang.android.mvvm_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding : ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+        ActivityMainBinding.inflate(layoutInflater) // Binding 활성화
     }
 
-    lateinit var userProfile: UserProfile
+    lateinit var userProfile: UserProfile // 지연초기화
+    var livedata = MutableLiveData<UserProfile>() // LiveData 선언
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        fetchUserProfile()
+        fetchUserProfile() // 초기 프로필 설정
+
+        val resultListener = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { // 액티비티로부터 오는 값을 받아옴
+            if (it.resultCode == Activity.RESULT_OK) {
+                val userProfile = livedata.value
+                userProfile?.phone = it.data?.getStringExtra("phone").toString()
+                userProfile?.name = it.data?.getStringExtra("name").toString()
+                livedata.value = userProfile // 밸류값 재설정 (옵저버 가동)
+            }
+        }
+
+        binding.edit.setOnClickListener {
+            val intent = Intent(this, EditUserProfileActivity::class.java)
+            intent.putExtra("phone", userProfile.phone)
+            intent.putExtra("name", userProfile.name)
+            resultListener.launch(intent)
+        }
+
+        livedata.observe(this) { // 변경사항 관찰하였을 때 UI 변경
+            Log.d("Main", "Observer ON")
+            updateUI(it)
+        }
+
+        if (livedata.value == null) { // livedata 존재하지 않을 시 기본 UI 설정
+            fetchUserProfile()
+        }
+
+
     }
 
     private fun fetchUserProfile() {
@@ -30,8 +58,7 @@ class MainActivity : AppCompatActivity() {
         userProfile.name = "홍길동"
         userProfile.phone = "01034567890"
         userProfile.address = "서울"
-
-        updateUI(userProfile)
+        livedata.value = userProfile
     }
 
     private fun updateUI(userProfile: UserProfile) {
